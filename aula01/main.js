7d8cfaa610be20a40b7bc5361ebf9eae8ccd43f9
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-07-02 17:21:59 -0300

    feat: botão funcionando + nome e nota

---
+++ aula01/main.js
@@ -0,0 +1,9 @@
+function botao_add(){
+    window.alert ('Tá funcionando')
+
+    var nome_atividade = window.document.getElementById ('nome_atividade')
+    var nota_atividade = window.document.getElementById ('nota_atividade')
+
+    window.alert (`O nome é: ${nome_atividade.value} e a nota é ${nota_atividade.value}`)
+}
+
