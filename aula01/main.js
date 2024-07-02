99a6f28c9cb952aac68caf55cc1c7d23b218a443
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-07-02 17:35:27 -0300

    feat: Add erros

--- aula01/main.js
+++ aula01/main.js
@@ -1,9 +1,15 @@
-function botao_add(){
-    window.alert ('Tá funcionando')
+function botao_add(e){
+    e.preventDefault();
 
     var nome_atividade = window.document.getElementById ('nome_atividade')
     var nota_atividade = window.document.getElementById ('nota_atividade')
 
-    window.alert (`O nome é: ${nome_atividade.value} e a nota é ${nota_atividade.value}`)
+    if (nome_atividade.value.length == 0 || nota_atividade.value.length == 0){
+        window.alert ('Você não preencheu nada, faça novamente!')
+    } else {
+        window.alert ('Tá funcionando')
+        window.alert (`O nome é: ${nome_atividade.value} e a nota é ${nota_atividade.value}`)
+    }
+    
 }
 
