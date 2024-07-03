53e1e8a223789320a079a2f919abdbe16af258d0
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-07-03 14:11:49 -0300

    feat: Tetando fazer com que calcule a media

--- aula01/main.js
+++ aula01/main.js
@@ -1,6 +1,7 @@
 var linhas = '';
 var imagem_aprovado = '<img src="imagens/emoje_festa.png" alt="Emoji festivo">'
 var imagem_reprovado = '<img src="imagens/emoji_triste.png" alt="Emoje triste">'
+var valor_media = window.document.getElementById ('media')
 
 function botao_add(e){
     e.preventDefault();
@@ -16,15 +17,21 @@ function botao_add(e){
         linha += `<td>${nota_atividade.value}</td>`
         linha += `<td>${nota_atividade.value >= 6? imagem_aprovado : imagem_reprovado}`
         linha += '</tr>';
-
+        // Isso aqui ta criando uma linha inteira com todas as informações
         linhas += linha;
-
+        var quantidade_notas = linhas.length
+        // Isso faz com que a gennte jogue cada vez mais "Linha", pois eu salvo o que ja tem no "Linhas" e acrescento cada vez mais "linha"
         var corpo_tabela = window.document.querySelector('tbody');
         corpo_tabela.innerHTML = linhas;
+
+        window.alert (`Quantidade de linhas: ${quantidade_notas}`)
 
         nome_atividade.value = ''
         nota_atividade.value = ''
     }
     
 }
+
+// Nesse momento aqui o meu var quantidade de notas ta contando simplesmente todos os caracteres que estão sendo lançados em linhas, dando valores de 90, 150 e por aí vai. Tenho que fazer de uma forma que ele conte apenas a quantidade de notas.
+
 
