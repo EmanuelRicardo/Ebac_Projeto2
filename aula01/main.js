9be8df6e8385e3e25ad3eef36d577e8f20af5e2c
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-07-03 13:39:18 -0300

    feat: linha juntando tudo

--- aula01/main.js
+++ aula01/main.js
@@ -1,3 +1,7 @@
+var linhas = '';
+var imagem_aprovado = '<img src="imagens/emoje_festa.png" alt="Emoji festivo">'
+var imagem_reprovado = '<img src="imagens/emoji_triste.png" alt="Emoje triste">'
+
 function botao_add(e){
     e.preventDefault();
 
@@ -7,8 +11,19 @@ function botao_add(e){
     if (nome_atividade.value.length == 0 || nota_atividade.value.length == 0){
         window.alert ('Você não preencheu nada, faça novamente!')
     } else {
-        window.alert ('Tá funcionando')
-        window.alert (`O nome é: ${nome_atividade.value} e a nota é ${nota_atividade.value}`)
+        var linha = '<tr>';
+        linha += `<td>${nome_atividade.value}</td>`
+        linha += `<td>${nota_atividade.value}</td>`
+        linha += `<td>${nota_atividade.value >= 6? imagem_aprovado : imagem_reprovado}`
+        linha += '</tr>';
+
+        linhas += linha;
+
+        var corpo_tabela = window.document.querySelector('tbody');
+        corpo_tabela.innerHTML = linhas;
+
+        nome_atividade.value = ''
+        nota_atividade.value = ''
     }
     
 }
