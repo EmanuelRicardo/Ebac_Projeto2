0d695fcf2070424c9384062f782196b098734d6c
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-07-04 23:48:12 -0300

    feat: aprovado e reprovado com cores

--- aula01/main.js
+++ aula01/main.js
@@ -3,6 +3,9 @@ var imagem_aprovado = '<img src="imagens/emoje_festa.png" alt="Emoji festivo">'
 var imagem_reprovado = '<img src="imagens/emoji_triste.png" alt="Emoje triste">'
 var valor_media = window.document.getElementById ('media')
 var contador_media = 0
+var valor_total_notas = 0
+var media = 0
+var passou_ou_nao = window.document.getElementById ('passou_ou_nao')
 
 
 function botao_add(e){
@@ -24,13 +27,26 @@ function botao_add(e){
         // Isso faz com que a gennte jogue cada vez mais "Linha", pois eu salvo o que ja tem no "Linhas" e acrescento cada vez mais "linha"
         if (nome_atividade.value.length != 0){
             contador_media += 1
-            window.alert (`contador: ${contador_media}`)
         } 
         // Isso aqui vai contar quantas vezes foi lançado um numero no "nota_atividade"
+        var nota_atividade_number = Number(nota_atividade.value)
+        valor_total_notas += nota_atividade_number
+
+        media = valor_total_notas / contador_media
+        valor_media.innerHTML = media.toFixed(1)
+
+
         var corpo_tabela = window.document.querySelector('tbody');
         corpo_tabela.innerHTML = linhas;
 
-        window.alert (`Quantidade de linhas: ${quantidade_notas}`)
+
+        if (media >= 6){
+            passou_ou_nao.innerHTML = 'Aprovado'
+            passou_ou_nao.style.backgroundColor = 'green'
+        } else {
+            passou_ou_nao.innerHTML = 'Reprovado'
+            passou_ou_nao.style.backgroundColor = 'red'
+        }
 
         nome_atividade.value = ''
         nota_atividade.value = ''
@@ -38,6 +54,4 @@ function botao_add(e){
     
 }
 
-// Nesse momento aqui o meu var quantidade de notas ta contando simplesmente todos os caracteres que estão sendo lançados em linhas, dando valores de 90, 150 e por aí vai. Tenho que fazer de uma forma que ele conte apenas a quantidade de notas.
-
-
+// preciso mudar agora aquele indicador de aprovado ou reprovado
